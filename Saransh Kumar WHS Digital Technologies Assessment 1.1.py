be84b79fd42ad41f97883c5398af375abc1f7ef8
Saransh Kumar <ciphongamessaranshk@gmail.com>
2024-03-13 23:22:02 +1300

    added labelling to the options
    I used enumerate to label the options, then converted the options to integers so the player could just type a option number to answer a question.

--- Saransh Kumar WHS Digital Technologies Assessment 1.1.py
+++ Saransh Kumar WHS Digital Technologies Assessment 1.1.py
@@ -1,31 +1,33 @@
 # I will be making a quiz program in Python.
+name = input("What's your name? ")
+
 QUESTIONS = {
     "When was the first known use of the word 'quiz'": [
-         "1781", "1771", "1871", "1881"
-    ],
-    "Which built-in function can get information from the user": [
-         "input", "get", "print", "write"
+        "1781", "1771", "1871", "1881"
     ],
     "Which keyword do you use to loop over a given list of elements": [
-        "Choices:", "for", "while", "each", "loop"
+        "for", "while", "each", "loop"
     ],
-    "What's the purpose of the built-in zip() function" : [
+    "What's the purpose of the built-in zip() function": [
         "To iterate over two or more sequences at the same time",
         "To combine several strings into one",
         "To compress several files into one archive",
         "To get information from the user",
     ],
+    "What's the name of Python's sorting algorithm": [
+        "Timsort", "Quicksort", "Merge sort", "Bubble sort"
+    ] # These are all the different choices for different questions.  
 }
-
-name = input("What's your name? ") # Asks name of player
 
 for question, alternatives in QUESTIONS.items():
     correct_answer = alternatives[0]
-    for alternative in sorted(alternatives):
-        print(f" Choice - {alternative}")
+    sorted_alternatives = sorted(alternatives)
+    for label, alternative in enumerate(sorted_alternatives): # enumerate is used to label each of the answers so the player does not have to type out the whole answer. They can just type on of the numbers.
+        print(f"  {label}) {alternative}")
 
-    answer = input(f"{question}?")
+    answer_label = int(input(f"{question}? ")) # use of int to convert the options into the number that labels an answer.
+    answer = sorted_alternatives[answer_label]
     if answer == correct_answer:
-        print("Correct! Good job, on to the next question!")
+        print("Correct!")
     else:
-        print(f"The answer is {correct_answer!r}, not {answer!r}! Try again next time! On to the next question!")
+        print(f"The answer is {correct_answer!r}, not {answer!r}")
