af7f7f4250204f912750a3ccb12d167d49f029c3
Saransh Kumar <ciphongamessaranshk@gmail.com>
2024-03-13 15:01:11 +1300

    started python quiz
    started with basic questions, put them in a list with a multiple choice system, where the first choice is correct (just for testing), tells the user what the answer was if they get it correct.

--- Saransh Kumar WHS Digital Technologies Assessment 1.1.py
+++ Saransh Kumar WHS Digital Technologies Assessment 1.1.py
@@ -1,1 +1,31 @@
-print ("Hello, World!")
+## will be making a program where you ask questions to the computer and it responds back with different answers and questions.
+QUESTIONS = {
+    "When was the first known use of the word 'quiz'": [
+        "1781", "1771", "1871", "1881"
+    ],
+    "Which built-in function can get information from the user": [
+        "input", "get", "print", "write"
+    ],
+    "Which keyword do you use to loop over a given list of elements": [
+        "for", "while", "each", "loop"
+    ],
+    "What's the purpose of the built-in zip() function": [
+        "To iterate over two or more sequences at the same time",
+        "To combine several strings into one",
+        "To compress several files into one archive",
+        "To get information from the user",
+    ],
+}
+
+name = input("What's your name? ") # Asks name of player
+
+for question, alternatives in QUESTIONS.items():
+    correct_answer = alternatives[0]
+    for alternative in sorted(alternatives):
+        print(f"  - {alternative}")
+
+    answer = input(f"{question}? ")
+    if answer == correct_answer:
+        print("Correct!")
+    else:
+        print(f"The answer is {correct_answer!r}, not {answer!r}")
