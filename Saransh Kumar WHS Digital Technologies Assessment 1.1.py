7ed4c72edd42e0837701b19e074cdc4c7f87473d
Saransh Kumar <ciphongamessaranshk@gmail.com>
2024-03-15 11:14:35 +1300

    quiz app has been made user friendly
    Application has been made user friendly by formatting and displaying questions in an easy to read and appropriate manner, countermeasures if one of the options has not been chosen. Will work on proper randomisation for each separate quiz session, using functions to organise code, and separating data into individual files.

--- Saransh Kumar WHS Digital Technologies Assessment 1.1.py
+++ Saransh Kumar WHS Digital Technologies Assessment 1.1.py
@@ -1,15 +1,19 @@
+# quiz.py
+
 # I will be making a quiz program in Python.
 # Will add up to 10 questions.
-name = input("What's your name? ")
 
+from string import ascii_lowercase
+import string
+import random
+
+name = input("What's your name? ")
+NUM_QUESTIONS_PER_QUIZ = 5
 QUESTIONS = {
     "When was the first known use of the word 'quiz'": [
         "1781", "1771", "1871", "1881"
     ],
-    "Which keyword do you use to loop over a given list of elements": [
-        "for", "while", "each", "loop"
-    ],
-    "What's the purpose of the built-in zip() function": [
+     "What's the purpose of the built-in zip() function": [
         "To iterate over two or more sequences at the same time",
         "To combine several strings into one",
         "To compress several files into one archive",
@@ -17,18 +21,34 @@ QUESTIONS = {
     ],
     "What's the name of Python's sorting algorithm": [
         "Timsort", "Quicksort", "Merge sort", "Bubble sort"
-    ] # These are all the different choices for different questions.  
+    ],
+    "What does dict.get(key) return if key isn't found in dict": [
+        "None", "key", "True", "False",
+    ]
 }
 
-for question, alternatives in QUESTIONS.items():
+num_questions = min(NUM_QUESTIONS_PER_QUIZ, len(QUESTIONS))
+questions = random.sample(list(QUESTIONS.items()), k=num_questions)
+
+num_correct = 0 # score 
+for num, (question, alternatives) in enumerate(questions, start=1): # starts the index with '1' instead of '0', and numbers the questions accordingly.
+    print(f"\nQuestion {num}:") 
+    print(f"{question}?")
     correct_answer = alternatives[0]
-    sorted_alternatives = sorted(alternatives)
-    for label, alternative in enumerate(sorted_alternatives): # enumerate is used to label each of the answers so the player does not have to type out the whole answer. They can just type on of the numbers.
+    labeled_alternatives = dict(
+        zip(ascii_lowercase, random.sample(alternatives, k=len(alternatives)))
+    ) # labels the alternatives in an alphabetically sorted choice list.
+    for label, alternative in labeled_alternatives.items():
         print(f"  {label}) {alternative}")
 
-    answer_label = int(input(f"{question}? ")) # use of int to convert the options into the number that labels an answer.
-    answer = sorted_alternatives[answer_label]
+    while (answer_label := input("\nChoice? ")) not in labeled_alternatives:
+        print(f"Please answer one of {', '.join(labeled_alternatives)}") # forces the user to choose between the choices if the have typed something else.
+
+    answer = labeled_alternatives[answer_label]
     if answer == correct_answer:
-        print("Correct!")
+        print("⭐ Correct! ⭐") # Adds a bit of flair.
+        num_correct += 1 # adds a point if correct.
     else:
         print(f"The answer is {correct_answer!r}, not {answer!r}")
+
+print(f"\nYou got {num_correct} correct out of {num} questions") # declares score when quiz is finished.
